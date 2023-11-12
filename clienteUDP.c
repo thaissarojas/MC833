fa54ef694bcdeee4ccdb55862ad244f520d010a2
Thaíssa Rojas <thay.rojasp@outlook.com>
2023-11-11 23:50:47 -0300

    added clienteUDP

--- clienteUDP.c
+++ clienteUDP.c
@@ -11,7 +11,6 @@
 #include <unistd.h>
 #include <time.h>
 #include <sys/socket.h>
-
 #define MAXLINE 4096
 #define MAXDATASIZE 100
 
@@ -20,44 +19,46 @@ flags, struct sockaddr *from, socklen_t *addrlen);
 
 ssize_t sendto(int sockfd, const void *buff, size_t nbytes,
 int flags, const struct sockaddr *to, socklen_t addrlen);
+void dg_cli(FILE *fp, int sockfd, const struct sockaddr *pservaddr, socklen_t servlen) {
+    int n;
+    char sendline[MAXLINE], recvline[MAXLINE + 1];
+
+    // fica aguardando comandos e mensagens enviadas pela linha de comando
+    // repete ate q conexao seja interrompida
+
+    while (fgets(sendline, MAXLINE, fp) != NULL) {
+
+        // envia msg para servidor
+        sendto(sockfd, sendline, strlen(sendline), 0, pservaddr, servlen);
+
+        // recebe msg do servidor
+        n = recvfrom(sockfd, recvline, MAXLINE, 0, NULL, NULL);
+        recvline[n] = 0; /* null terminate */
+
+        // envia para saida a resposta recebida
+        fputs(recvline, stdout);
+    }
+}
+
 
 int main(int argc, char **argv) {
+    int sockfd; 
+    struct sockaddr_in servaddr; 
+    if(argc != 2) 
+        perror("usage: udpcli <IPaddress>"); 
 
-    int    sockfd, n;
-    char buff[MAXLINE];
-    char sendline[MAXLINE], recvline[MAXLINE];
+    // configuracao das portas, tipo de conexao, etc da estrutura do servaddr
+    bzero(&servaddr, sizeof(servaddr)); 
+    servaddr.sin_family = AF_INET; 
+    servaddr.sin_port = htons(5002); 
+    inet_pton(AF_INET, argv[1], &servaddr.sin_addr);
 
-    if(argc != 2){
-        perror("usage: udpcli <IPaddress>");
-    }
+    // setando valores do socket para UDP
+    sockfd = socket(AF_INET, SOCK_DGRAM, 0); 
+    
 
-    if ( (sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
-        perror("socket error");
-        exit(1);
-    } else{
-        perror("socket criado");
-    }
-
-
-    //setando server address
-    struct sockaddr_in servaddr;
-    servaddr.sin_family = AF_INET;
-    servaddr.sin_port = htons(5002); /* daytime server */
-    //convertendo argumento 1 em string no ripo af_inet e salva em &servaddr.sin_addr)
-    if (inet_pton(AF_INET, argv[1], &servaddr.sin_addr) <= 0) {
-        perror("inet_pton error");
-        exit(1);
-    }
-
-
-
-    char   buf[MAXDATASIZE];
-    int fechar = -1;
-    /// escutando oq o servidor tem a dizer
-    while (fgets(sendline, MAXLINE, fp) != NULL) {
-        sendto(sockfd, sendline, strlen(sendline), 0, pservaddr, servlen);
-        n = Recvfrom(sockfd, recvline, MAXLINE, 0, NULL, NULL);
-        recvline[n] = 0;
-        Fputs(recvline, stdout);
-    }
+    // dg cria socket UDP para conexao do cliente com servidor
+    dg_cli(stdin, sockfd, (struct sockaddr *) &servaddr, sizeof(servaddr)); 
+	exit(0); 
+    
 }
