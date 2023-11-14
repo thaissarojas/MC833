8661e546c7ed750dd7986fdbe52e933b892d5de8
Thaissa Rojas Pereira <ra224714@suriname.lab.ic.unicamp.br>
2023-11-14 18:07:13 -0300

    added server and clientTCP

--- clientTCP.c
+++ clientTCP.c
@@ -9,11 +9,58 @@
 #include <string.h>
 #include <stdlib.h>
 #include <unistd.h>
-
+#include <stdbool.h>
 #include <time.h>
 
 #define MAXLINE 4096
 #define MAXDATASIZE 100
+
+ssize_t /* Write "n" bytes to a descriptor. */
+ writen(int fd, const void *vptr, size_t n)
+ {
+    size_t nleft;
+    ssize_t nwritten;
+    const char *ptr;
+    ptr = vptr;
+    nleft = n;
+    while (nleft > 0) {
+        if ( (nwritten = write(fd, ptr, nleft)) <= 0) {
+            if (nwritten < 0 && errno == EINTR)
+                nwritten = 0; /* and call write() again */
+            else
+                return (-1); /* error */
+        }
+        nleft -= nwritten;
+        ptr += nwritten;
+    }
+    return (n);
+ }
+/* PAINFULLY SLOW VERSION -- example only */
+ ssize_t
+ readline(int fd, void *vptr, size_t maxlen)
+ {
+    ssize_t n, rc;
+    char c, *ptr;
+    ptr = vptr;
+    for (n = 1; n < maxlen; n++) {
+        again:
+        if ( (rc = read(fd, &c, 1)) == 1) {
+            *ptr++ = c;
+            if (c == '\n')
+            break; /* newline is stored, like fgets() */
+        } else if (rc == 0) {
+            *ptr = 0;
+            return (n - 1); /* EOF, n - 1 bytes were read */
+        } else {
+            if (errno == EINTR)
+                goto again; 
+            return (-1); /* error, errno set by read() */
+        }
+    }
+    *ptr = 0; /* null terminate like fgets() */
+    return (n);
+    }
+
 
 int main(int argc, char **argv) {
 
@@ -21,6 +68,7 @@ int main(int argc, char **argv) {
     int    sockfd, n;
     char buff[MAXLINE];
 
+    // perror("cuuu %d %d", &argv[0],&argv[1]);
     if ( (sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
         perror("socket error");
         exit(1);
@@ -32,10 +80,10 @@ int main(int argc, char **argv) {
     //setando server address
     struct sockaddr_in servaddr;
     servaddr.sin_family = AF_INET;
-    servaddr.sin_port = htons(5000); /* daytime server */
+    servaddr.sin_port = htons( atoi(argv[1])); /* daytime server */
     servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
     //convertendo argumento 1 em string no ripo af_inet e salva em &servaddr.sin_addr)
-    if (inet_pton(AF_INET, argv[1], &servaddr.sin_addr) <= 0) {
+    if (inet_pton(AF_INET, argv[2], &servaddr.sin_addr) <= 0) {
         perror("inet_pton error");
         exit(1);
     }
@@ -47,31 +95,42 @@ int main(int argc, char **argv) {
         perror("connect");
     }
         const char* ip  = inet_ntop(AF_INET, &servaddr.sin_addr, buff, sizeof(buff));
-        char client_port_string[] = "Porta= 5000 IP=";
+        char client_port_string[] = "Porta e IP=";
+        
         strcat(client_port_string,ip);
         perror(client_port_string);
 
-    char   buf[MAXDATASIZE];
-    int fechar = -1;
-    /// escutando oq o servidor tem a dizer
-    while ( (n = read(sockfd, recvline, MAXLINE)) > 0) {
-        recvline[n] = 0;
-        if (fputs(recvline, stdout) == EOF) {
-            perror("fputs error");
-            // exit(1);
-        } else {
-                // verifica se o servidor mandou desconectar
-                fechar = strcmp(recvline, "DESCONECTAR");
-                if(fechar==0){
-                    exit(1);
-                } else {
-                    //envio de feedback para o servidor
-                    snprintf(buf, sizeof(buf), " SIMULACAO: CPU_INTENSIVA CONCLUÍDA");
-                    write(sockfd, buf, strlen(buf));
-                    sleep(1);
-                }
+    char   buf[MAXDATASIZE], sendline[MAXLINE], input[MAXLINE];
+    int fechar = -1,stdineof,maxfdp1;
+    FILE *fp = fopen(argv[1],"r");
+    while (fgets(input, sizeof(input), stdin) != NULL) {
+        printf("%s", input);
+    }
+    
+    fd_set rset;
+    stdineof = 0;
+    FD_ZERO(&rset);
+    for ( ; ; ) {
+        FD_SET(fileno(fp), &rset);
+        FD_SET(sockfd, &rset);
+        maxfdp1 = (fileno(fp) > sockfd ? fileno(fp) : sockfd )+ 1;
+        select(maxfdp1, &rset, NULL, NULL, NULL);
+        
+        if(FD_ISSET(sockfd,&rset)){
+            if (readline(sockfd,recvline,MAXLINE)==0){
+                perror("str_cli: server terminated prematurely");
+            }
+            fputs(recvline,stdout);
+        }
+
+        if(FD_ISSET(fileno(fp),&rset)){
+            if(fgets(sendline,MAXLINE,fp)==NULL)
+                break;
+            
+            writen(sockfd,sendline,strlen(sendline));
         }
     }
+
     if (n < 0) {
         perror("read error");
         // exit(1);
--- server.c
+++ server.c
@@ -1,4 +1,5 @@
 #include <sys/socket.h>
+#include <sys/select.h>
 #include <sys/types.h>
 #include <arpa/inet.h>
 #include <netinet/in.h>
@@ -8,13 +9,70 @@
 #include <errno.h>
 #include <string.h>
 #include <stdlib.h>
+#include <sys/time.h>
 #include <unistd.h>
 #include <time.h>
 
 #define LISTENQ 10
 #define MAXLINE 4096
 
-#define MAXDATASIZE 100
+// Função select para 
+ssize_t writen(int fd, const void *vptr, size_t n) {
+    size_t nleft;
+    ssize_t nwritten;
+    const char *ptr;
+    ptr = vptr;
+    nleft = n;
+    while (nleft > 0) {
+        if ( (nwritten = write(fd, ptr, nleft)) <= 0) {
+            if (nwritten < 0 && errno == EINTR)
+                nwritten = 0; /* and call write() again */
+            else
+                return (-1); /* error */
+        }
+        nleft -= nwritten;
+        ptr += nwritten;
+    }
+    return (n);
+}
+
+void str_cli(FILE *fp, int sockfd) {
+    int maxfdp1, n, stdineof;
+    fd_set rset;
+    char sendline[MAXLINE], recvline[MAXLINE], buf[MAXLINE];
+    stdineof = 0;
+    FD_ZERO(&rset);
+    for ( ; ; ) {
+        if(stdineof==0)
+            FD_SET(fileno(fp), &rset);
+        FD_SET(sockfd, &rset);
+        maxfdp1 = (fileno(fp) > sockfd ? fileno(fp) : sockfd )+ 1;
+        select(maxfdp1, &rset, NULL, NULL, NULL);
+        
+        if(FD_ISSET(sockfd,&rset)){
+            if( (n = read(sockfd, buf, MAXLINE)) == 0){
+                if(stdineof=1)
+                    return;
+                else    
+                    perror("str_cli: server terminated prematurely");
+            }
+            write(fileno(stdout),buf,n);
+        }
+
+        if(FD_ISSET(fileno(fp),&rset)){
+            if((n=read(fileno(fp),buf,MAXLINE))==0){
+                stdineof = 1;
+                shutdown(sockfd,SHUT_WR);
+                FD_CLR(fileno(fp),&rset);
+                continue;
+            }
+            write(sockfd,buf,n);
+        }
+    }
+}
+
+
+
 
 void printFile(const char *msg){
 
@@ -27,11 +85,16 @@ int main(int argc, char **argv) {
     //Settando struct
     char buff[MAXLINE];
     struct sockaddr_in servaddr, cliaddr;
+    int i, maxi, maxfd, listenfd, connfd, sockfd, client[FD_SETSIZE], nready;
+    ssize_t n;
+    socklen_t clilen;
+    fd_set rset,allset;
+    const int on = 1;
 
-    //settando listenfd
-    int listenfd = socket(AF_INET, SOCK_STREAM, 0);
+    //settando listenfdW
+    listenfd = socket(AF_INET, SOCK_STREAM, 0);
     servaddr.sin_family = AF_INET;
-    servaddr.sin_port = htons(5000); /* daytime server */
+    servaddr.sin_port = htons(a); /* daytime server */
     servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
     //bind c socket
 
@@ -51,85 +114,92 @@ int main(int argc, char **argv) {
         perror("- listen tbm funcionou");
     }
 
-    int connfd, n;
-    char   buf[MAXDATASIZE];
+    // setando valores de clientes para criar array para rastreamento
+    maxfd = listenfd;
+    maxi = -1;
+    
+    for (i=0; i<FD_SETSIZE; i++){
+        client[i] = -1;
+    }
+    FD_ZERO(&allset);
+    FD_SET(listenfd,&allset);
+
+
+    
+    char   buf[MAXLINE];
     // time_t ticks;
 
     for ( ; ; ) {
+        rset = allset;
+        nready = select(maxfd + 1,&rset,NULL,NULL,NULL);
+
+
         // SETANDO TEMPO
         time_t ticks;
         ticks = time(NULL); 
 
         //SETANDO VARIAVEIS PARA GUARDAR INFORMAÇÕES DO CLIENTE
         socklen_t len = sizeof(cliaddr);
-
-        if ((connfd = accept(listenfd, (struct sockaddr *) &cliaddr, &len)) == -1 ) {
-            perror("accept" );
-            exit(1);
-        }
-
-        // se o resultado do fork for 0, é filho
-        pid_t pid;
-        if((pid = fork())==0){
-            close(listenfd);
-            snprintf(buff, sizeof(buff), "Processo= %d",getpid());
-
-
-            printFile(buff);
-            char client_port_string[MAXDATASIZE] = "Porta = ";
-            snprintf(buff, sizeof(buff), "[ %.24s] Conexão com cliente com  ", ctime(&ticks));
-
-
-            const char* client_ip;
-            int client_port ;
-            char clientInfo[MAXDATASIZE] = "IP = ";
-            //definição de porta e ip do cliente 
-            client_ip  = inet_ntop(AF_INET, &cliaddr.sin_addr, buff, sizeof(buff));
-            client_port = ntohs(cliaddr.sin_port);
-
-            //imprimindo string no arquivo
-            strcat(clientInfo,client_ip);
-            strcat(clientInfo," Porta= ");
-            sprintf(client_port_string, "%d", client_port);
-            strcat(clientInfo,client_port_string);
-            printFile(clientInfo);
-
-
-            //ENVIANDO AO CLIENTE COMANDO
-            snprintf(buf, sizeof(buf), "SIMULE: CPU_INTENSIVA\n ");
-            write(connfd, buf, strlen(buf));
-
-            //ESCREVENDO NO ARQUIVO AÇÃO
-            snprintf(buf, sizeof(buf), "[ %.24s] Comando enviado: SIMULE: CPU_INTENSIVA", ctime(&ticks));
-            printFile(buf);
-
-            //LEITURA PARA VER SE RECEBEU ALGUMA INFORMAÇÃO
-            char   recvline[MAXLINE + 1];
-            while ( (n = read(connfd, recvline, MAXLINE)) > 0 ) {
-                recvline[n] = 0;
-                // coloca no arquivo o que foi recebido pelo cliente
-                snprintf(buf, sizeof(buf), "[ %.24s] Recebido pelo cliente: ", ctime(&ticks));
-                strcat(buf,clientInfo);
-                strcat(buf,recvline);
-                printFile(buf);
-                if (fputs(recvline, stdout) == EOF) {
-                    perror("fputs error");
-                    // exit(1);
-                } 
-                // envia comando de desconectar para o cliente
-                snprintf(buf, sizeof(buf), "DESCONECTAR");
-                write(connfd, buf, strlen(buf));
-                snprintf(buf, sizeof(buf), "[ %.24s] Conexão encerrada com cliente ", ctime(&ticks));
-                strcat(buf,clientInfo);
-                printFile(buf);
+        if(FD_ISSET(listenfd,&rset)){
+            // conexão com cliente
+            clilen = sizeof(cliaddr);
+            if ((connfd = accept(listenfd, (struct sockaddr *) &cliaddr, &clilen)) == -1 ) {
+                perror("accept" );
+                exit(1);
             }
-            exit(0);
-            close(connfd);
+            
+            // coloca conexão para cliente
+            for (i=0; i<FD_SETSIZE; i++){
+                if(client[i] < 0){
+                    client[i] = connfd;
+                    break;
+                }
+            }
+
+            if(i==FD_SETSIZE){
+                perror("acabou limite de clientes");
+            }
+
+            if(connfd>maxfd){
+                maxfd = connfd;
+            }
+
+            if(i>maxi){
+                maxi = i;
+            }
+
+            if(--nready<=0){
+                continue;
+            }
 
         }
-        close(connfd);
+        for (i=0; i<maxi; i++){
+            if((sockfd=client[i] ) < 0 ){
+                continue;
+            }
+
+            if(FD_ISSET(sockfd,&rset)){
+                // acabou conexoes
+                if( (n = read(sockfd, buf, MAXLINE)) == 0){
+                    close(sockfd);
+                    FD_CLR(sockfd,&allset);
+                    client[i]=-1;
+                }
+                else{
+                    writen(fileno(stdout),buf,n);
+                }
+
+                if(--nready<=0){
+                    break;
+                }
+            }
+
+        }
+
+
     }
 
     return(0);
 
 }
+
